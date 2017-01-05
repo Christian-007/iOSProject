f9e1522d81dd8ed358d160ba23b9ac900d48025a
Christian Ing <cisunardi1@sheffield.ac.uk>
2017-01-05 11:29:32 +0000

    Initial Commit

---
+++ LiftLog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LiftLog
+//
+//  Created by Christian Ing on 04/01/2017.
+//  Copyright © 2017 Christian Ing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
